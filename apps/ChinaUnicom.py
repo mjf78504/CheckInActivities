bebb62c745269a8f2e772c528eecfb2a1dd408c0
mjf78504 <admin@xinwl.com>
2019-12-23 21:04:25 +0800

    Update ChinaUnicom.py

--- apps/ChinaUnicom.py
+++ apps/ChinaUnicom.py
@@ -112,6 +112,7 @@ class ChinaUnicomApp:
             #获取抽奖usernumberofjsp
             usernumberofjsp_url = 'http://m.client.10010.com/dailylottery/static/textdl/userLogin'
             usernumberofjsp_req = self.session.post(url=usernumberofjsp_url).text()
+            print(usernumberofjsp_req)
             usernumberofjsp_search = re.search(r'[a-zA-Z0-9]{32}',usernumberofjsp_req).group()
             print(usernumberofjsp_search)
             # 访问访问Weibo获取金币
