9139720dd24a8c33d2cfcaf53e19f39a4bbd550b
mjf78504 <admin@xinwl.com>
2019-12-27 14:23:30 +0800

    Update ChinaUnicom.py

--- apps/ChinaUnicom.py
+++ apps/ChinaUnicom.py
@@ -123,7 +123,7 @@ class ChinaUnicomApp:
             print(qylogin_req)
             qysign_url = 'https://qy.chinaunicom.cn/mobile/userarea/queryAccountInfo'
             qysign_req = self.session.get(qysign_url).json()
-            print(qysign_req.cookies)
+            print(qysign_req)
             exit()
             # 每日免费抽奖
             print('---每天免费抽奖三次情况记录---')
