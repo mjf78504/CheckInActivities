56abf1c4f87d8b1752d2fd37985740c579856ab6
mjf78504 <admin@xinwl.com>
2020-01-02 19:42:33 +0800

    Update ChinaUnicom.py

--- apps/ChinaUnicom.py
+++ apps/ChinaUnicom.py
@@ -123,7 +123,14 @@ class ChinaUnicomApp:
             qy_req = self.session.post(url=qy_url, data=qy_data, allow_redirects=False)
             self.session.cookies.clear()
             print(self.session.cookies.get_dict())
-            qy_cookies = self.session.get(url=account_url)
+            # qy_cookies = self.session.get(url=account_url)
+            cookies = {
+                'CACHE_JSESSIONID': '9360536E958F48A098A7E4411A8397ED',
+                'Hm_lvt_e080bb1a9f98b31badca3d6f6464d7c2': '1577453974,1577459700,1577542109,1577542138',
+                'remember_me': 'd14d7880-ec2c-49fa-898d-2afb61bdeb4e',
+                'Hm_lpvt_e080bb1a9f98b31badca3d6f6464d7c2': int(time.time()),
+            }
+            self.session.cookies = cookies
             print(self.session.cookies.get_dict())
             qylogin_req = self.session.get(url=qy_req.headers['Location'])
             qytest_url = 'https://qy.chinaunicom.cn/mobile/userarea/queryAccountInfo'
