791e34e327ce4f14ab53bf53c3ae9d24934a520a
mjf78504 <admin@xinwl.com>
2020-01-03 17:23:09 +0800

    Update ChinaUnicom.py

--- apps/ChinaUnicom.py
+++ apps/ChinaUnicom.py
@@ -254,13 +254,8 @@ class ChinaUnicomApp:
         qy_cookies = self.session.get(url=account_url)
         uuid_msg = str(uuid.uuid1())
         print(uuid_msg)
-        uuid_ms = re.findall(r'[(](.*?)[)]',uuid.uuid1()) 
-        print(uuid_ms)
-        exit()
         self.session.cookies.set('remember_me',uuid_msg)
-        print(self.session.cookies.get_dict())
         qylogin_req = self.session.get(url=qy_req)
-        print(self.session.cookies.get_dict())
         # 权益中心首页礼品
         qyhome_url = 'https://qy.chinaunicom.cn/mobile/lottery/doLo?actId=1000000000012802'
         qyhome_msg = self.session.get(url=qyhome_url).json()
