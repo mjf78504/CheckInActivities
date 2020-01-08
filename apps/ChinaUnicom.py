968c51ebaa70c7479bb2bc7a1c5eed03bc8547fc
mjf78504 <admin@xinwl.com>
2020-01-08 17:15:34 +0800

    Update ChinaUnicom.py

--- apps/ChinaUnicom.py
+++ apps/ChinaUnicom.py
@@ -311,7 +311,8 @@ class ChinaUnicomApp:
                 print(getgame_req['msg'])
                 break
             game_data = getgame_req['data']
-            for key in game_data:
+            gamesf_data = random.shuffle(game_data)
+            for key in gamesf_data:
                 game_url = 'https://qy.chinaunicom.cn/mobile/sb/findingboom?actId=86DFB114DF454D389B0AB2E18A730C99C5F56031D8DF9115&tradeId=' + getgame_req['tradeId'] + '&boomId=' + key + '&channelType=10086'
                 game_req = self.session.get(url=game_url).json()
                 if game_req['msg'] == 'success':
